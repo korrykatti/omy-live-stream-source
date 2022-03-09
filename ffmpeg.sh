93630d55b13ecefff8019d3ecd2ba9fd7141e718
KorryKatti <60970528+korrykatti@users.noreply.github.com>
2022-03-09 12:19:37 +0530

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -15,5 +15,5 @@ KEY="$KEY"                                     # Clé à récupérer sur l'event
 ffmpeg \
     -stream_loop -1 -i "$SOURCE" -deinterlace \
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
-    -acodec libmp3lame -ar 44100 -threads 6 -crf 3 -b:a 712000 -bufsize 512k \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
     -f flv "$YOUTUBE_URL/$KEY"
